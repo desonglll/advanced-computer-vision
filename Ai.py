d400e4e5208513991f1a4100a8674ec59aa0fb4b
desonglll <lindesong666@163.com>
2024-11-16 20:53:26 +0800

    Update program.

--- Ai.py
+++ Ai.py
@@ -1,46 +1,82 @@
-import json
-import os
-
+import numpy as np
+import time
+from tensorflow.keras.models import load_model
 import cv2
 from mediapipe_impl.pose_estimation import PoseEstimationModule as pm
+import mediapipe as mp
 
-# cap = cv2.VideoCapture(0)
+mp_draw = mp.solutions.drawing_utils
+mp_pose = mp.solutions.pose
+pose = mp_pose.Pose()
 detector = pm.PoseDetector()
 
-images_dir = "./datasets/img"
-images = [os.path.join(images_dir, img) for img in os.listdir(images_dir) if img.endswith(('.png', '.jpg', '.jpeg'))]
+# 加载已保存的模型
+model = load_model('./models/pose_estimation.keras')
 
-for image_path in images:
-    print(image_path)
-    # success, img = cap.read()
-    # img = cv2.resize(img, (1280, 720))
-    img = cv2.imread(image_path)
-    img = detector.find_pose(img, draw=False)
-    lm_list = detector.find_position(img, False)
+# 验证模型是否加载成功
+model.summary()
+
+ALL_LABELS = ['Blur'], ['Normal'], ['Wrong']
+cap = cv2.VideoCapture(1)
+
+p_time = 0
+while True:
+    success, img = cap.read()
+    img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = pose.process(img_rgb)
+
+    # print(results.pose_landmarks)
+    if results.pose_landmarks:
+        mp_draw.draw_landmarks(img, results.pose_landmarks, mp_pose.POSE_CONNECTIONS)
+        data = []
+        for id, lm in enumerate(results.pose_landmarks.landmark):
+            h, w, c = img.shape
+            # print(id, lm.x)
+            data.extend([lm.x, lm.y, lm.z, lm.visibility])
+            cx, cy = int(lm.x * w), int(lm.y * h)
+            cv2.circle(img, (cx, cy), 10, (255, 0, 0), cv2.FILLED)
+        X = np.array(data).reshape((1, 33, 4))
+        print(X.shape)
+
+        predictions = model.predict(X)
+
+        # 获取预测类别的索引
+        predicted_class = np.argmax(predictions, axis=1)[0]
+
+        print(f"Predicted Class: {predicted_class}")
+        print(ALL_LABELS[predicted_class])
+
+    c_time = time.time()
+    fps = 1 / (c_time - p_time)
+    p_time = c_time
+
+    cv2.putText(img, 'FPS: {:.2f}'.format(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+
     cv2.imshow('img', img)
-    cv2.waitKey(1)  # 等待1毫秒刷新窗口
-
-    # label = input(f"Please enter the label (0/1): ")
-    data = {
-        "image": image_path,
-        "features": lm_list,
-        "label": 1
-    }
-
-    # 读取现有JSON文件（如果存在），将新数据追加到列表中
-    json_file_path = "data.json"
-    try:
-        with open(json_file_path, "r") as file:
-            data_list = json.load(file)
-    except FileNotFoundError:
-        data_list = []
-
-    # 追加新数据
-    data_list.append(data)
-
-    # 将更新后的列表写回JSON文件
-    with open(json_file_path, "w") as file:
-        json.dump(data_list, file, indent=4)
-
-    # 关闭窗口并退出循环
-    cv2.destroyAllWindows()
+    cv2.waitKey(1)
+# while True:
+#     success, img = cap.read()
+#     img = detector.find_pose(img=img)
+#     lm_list = detector.find_position(img, draw=False)
+#     data = []
+#     for keypoint in lm_list:
+#         data.extend([keypoint["x"], keypoint["y"], keypoint["z"], keypoint["visibility"]])
+#     X = np.array(data).reshape((1, 33, 4))
+#     print(X.shape)
+#     # 模型预测
+#     predictions = model.predict(X)
+#
+#     # 获取预测类别的索引
+#     predicted_class = np.argmax(predictions, axis=1)[0]
+#
+#     print(f"Predicted Class: {predicted_class}")
+#     print(ALL_LABELS[predicted_class])
+#
+#     c_time = time.time()
+#     fps = 1 / (c_time - p_time)
+#     p_time = c_time
+#
+#     cv2.putText(img, 'FPS: {:.2f}'.format(int(fps)), (70, 50), cv2.FONT_HERSHEY_PLAIN, 3, (255, 0, 0), 3)
+#
+#     cv2.imshow('img', img)
+#     cv2.waitKey(1)
